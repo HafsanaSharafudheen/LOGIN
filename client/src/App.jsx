81506133d4406bf93dcb5666d25aa49d6b75c0db
hafsana <hafsanasharafudheen@gmail.com>
2024-03-10 15:47:36 +0530

    add routesa nd create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
---
+++ client/src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {BroserRouter,Routes,Route} from 'react-router-dom'
+function About() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default About
