e16f6a5d0e6341550648baa708606c42306f7c29
hafsana <hafsanasharafudheen@gmail.com>
2024-03-11 08:09:58 +0530

    create signup api

--- api/index.js
+++ api/index.js
@@ -1,17 +1,25 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/user.route.js'
+
 import dotenv from 'dotenv';
 dotenv.config();
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log('Connected to Mongodb');
-}).catch((err)=>{
-    console.log(err);
-}
+var c="mongodb+srv://hafsanasharafudheen:hafsanasharafudheen@mern.brrfyaa.mongodb.net"
+mongoose.connect(c).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('MongoDB connection error:', err);
+});
 
 
 
-)
+
 const app=express();
+app.use(express.json());
+
 app.listen(3000,()=>{
     console.log("server listening on port 3000");
 })
+app.use('/api/user',userRoutes);
+app.use('api/auth',authRoutes);
